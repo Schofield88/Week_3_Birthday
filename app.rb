c34f255c7e8fd4c5c1376c7d33da01f37b53ab56
Luke Phyall <lukeprospect@yahoo.com>
2019-02-21 12:43:16 +0000

    Adds some stuff the last commit missed

--- app.rb
+++ app.rb
@@ -6,5 +6,12 @@ class Birthday < Sinatra::Base
     erb :index
   end
 
+  get '/birthday' do
+    p params
+    @name = params[:name]
+    @birthday = params[:birthday]
+    erb :birthday
+  end
+
   run! if app_file == $0
 end
